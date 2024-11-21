76fc4481fd82e3379944294c3407bff62b8a1186
Quantr10 <thequantran2005@gmail.com>
2024-11-21 10:42:39 -0500

    update firebase

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7YV9pMoocKdbQHbu_EAcRIpy7_eDb5tw",
-  authDomain: "paw-ride-2024.firebaseapp.com",
-  projectId: "paw-ride-2024",
-  storageBucket: "paw-ride-2024.firebasestorage.app",
-  messagingSenderId: "1015353413843",
-  appId: "1:1015353413843:web:4c3fbf1b61d3da587d4426",
-  measurementId: "G-XYFTHJQYSH"
+  apiKey: window.FIREBASE_API_KEY,
+  authDomain: window.FIREBASE_AUTH_DOMAIN,
+  projectId: window.FIREBASE_PROJECT_ID,
+  storageBucket: window.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: window.FIREBASE_MESSAGING_SENDER_ID,
+  appId: window.FIREBASE_APP_ID,
+  measurementId: window.FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
