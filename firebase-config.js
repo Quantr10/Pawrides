81b8c544a4d965116da3d5a8d62c5d3c92107d7a
Quantr10 <thequantran2005@gmail.com>
2024-11-21 10:22:57 -0500

    update env

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
+  apiKey: "AIzaSyC7YV9pMoocKdbQHbu_EAcRIpy7_eDb5tw",
+  authDomain: "paw-ride-2024.firebaseapp.com",
+  projectId: "paw-ride-2024",
+  storageBucket: "paw-ride-2024.firebasestorage.app",
+  messagingSenderId: "1015353413843",
+  appId: "1:1015353413843:web:4c3fbf1b61d3da587d4426",
+  measurementId: "G-XYFTHJQYSH"
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
